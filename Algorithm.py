d3cf1e69d143a423c50ad0b88e0ab7c853b19eed
AY7 <ay7inc@gmail.com>
2018-10-01 11:58:47 +0200

    new commit

--- Algorithm.py
+++ Algorithm.py
@@ -3,5 +3,6 @@ class Algorithm(object):
 
     def ___main__(self):
         func = function()
-        # hello world, this is a git hub svc test 
+        # hello world, this is a git hub svc
+        # test
         return func
